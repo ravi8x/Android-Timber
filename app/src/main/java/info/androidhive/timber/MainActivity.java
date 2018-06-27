a268ea509a17e3ffd3b09442e0acfd31bbc126b4
Ravi Tamada <ravi8x@gmail.com>
2018-06-27 10:21:41 +0530

    Basic timber setup is done

---
+++ app/src/main/java/info/androidhive/timber/MainActivity.java
@@ -0,0 +1,23 @@
+package info.androidhive.timber;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Timber.d("Hello");
+
+        Timber.e("This is error!");
+    }
+}
---
+++ app/src/main/java/info/androidhive/timber/log/ReleaseTree.java
@@ -0,0 +1,10 @@
+package info.androidhive.timber.log;
+
+import timber.log.Timber;
+
+public class ReleaseTree extends Timber.Tree {
+    @Override
+    protected void log(int priority, String tag, String message, Throwable t) {
+        // implement your logic
+    }
+}
